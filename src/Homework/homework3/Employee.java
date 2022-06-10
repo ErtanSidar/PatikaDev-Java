255f74d5f9310e64c7bf45e9f2e36fa261a87842
ErtanSidar <essoft3@gmail.com>
2022-06-10 14:52:38 +0300

    homeworks solved 6.10.2022

---
+++ src/Homework/homework3/Employee.java
@@ -0,0 +1,54 @@
+package Homework.homework3;
+
+import java.util.Date;
+
+public class Employee {
+	
+	String nameSurname;
+	double salary;
+    int workHours;
+    int hireYear;
+    
+	public Employee(String nameSurname, double salary, int workHours, int hireYear) {
+		super();
+		this.nameSurname = nameSurname;
+		this.salary = salary;
+		this.workHours = workHours;
+		this.hireYear = hireYear;
+	}
+	
+	public void tax() {
+		if(this.salary<1000) {
+			System.out.println("Vergi uygulamak için yeterli değildir");
+		}else {
+			System.out.println(salary*0.03 +" vergi uygulanmıştır");
+		}
+	}
+	
+	public void bonus() {
+		if(workHours>40) {
+			System.out.println((workHours-40)*30 +"tl bonus eklenmiştir");
+		}
+	}
+	
+	public void raiseSalary() {
+		int newdate=new Date().getYear();
+		if(newdate-hireYear<10) {
+			salary=salary+salary*0.05;
+			System.out.println("Yeni maaş :"+salary);
+		}else if (newdate-hireYear>9 && newdate-hireYear<25) {
+			salary=salary+salary*0.10;
+			System.out.println("Yeni maaş :" +salary);
+		}else {
+			salary=salary+salary*0.15;
+			System.out.println("Yeni maaş :" +salary);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [nameSurname=" + nameSurname + ", salary=" + salary + ", workHours=" + workHours
+				+ ", hireYear=" + hireYear + "]";
+	}
+	
+}
---
+++ src/Homework/homework3/Main.java
@@ -0,0 +1,14 @@
+package Homework.homework3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp= new Employee("Ertan Sidar", 2500, 50, 1997);
+		emp.tax();
+		emp.bonus();
+		emp.raiseSalary();
+		System.out.println(emp);
+	}
+
+}
